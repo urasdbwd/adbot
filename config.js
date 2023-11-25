c36270680427b232a44e9ca16fe47a833e9b3d5f
urasdbwd <extratoppingsplease123@gmail.com>
2023-11-25 10:20:05 -0800

    oops config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    "username": "mcbrickbosss",
-    "invite": "discord.gg/bwp",
-    "webhook": "https://discord.com/api/webhooks/1167883945271107637/cat5n3naCZ7nmQ4mWa39Fm2dpyN7ktubj8JKurYwwZWl91AHtbsbyh7lu3QzZqfRJKX1",
+    "username": null,
+    "invite": null,
+    "webhook": null,
     "pfp": "https://cdn.discordapp.com/avatars/742262465743814697/1151e50f47596386fea29ad21759d479.png?size=2048",
-    "key": "2032936cc03d1c4e148f2fb499869544bote3b0866e8515d71a8ae13f2114c4813c93523b16f87133a5843865eae55d0d8a"
+    "key": null
 }
